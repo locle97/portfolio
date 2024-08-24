c1e400634c3ffd03cebf50269ba33e146faec6a9
Loc Le <lpl212757@gmail.com>
2024-08-24 17:38:08 +0700

    feat - disable animated cursor

--- src/App.jsx
+++ src/App.jsx
@@ -41,20 +41,6 @@ function App() {
 
   return (
     <>
-      <AnimatedCursor
-        innerSize={8}
-        outerSize={35}
-        innerScale={1}
-        color='0, 0, 0'
-        outerScale={2}
-        outerAlpha={0}
-        hasBlendMode={true}
-        innerStyle={{
-          backgroundColor: '#333'
-        }}
-        outerStyle={{
-          border: '3px solid #333'
-        }} />
       <Particles
         id="tsparticles"
         particlesLoaded={particlesLoaded}
