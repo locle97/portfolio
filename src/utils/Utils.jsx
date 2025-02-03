268b0e541add240ed902dcb0de759f9b5195b29a
Loc Le <lpl212757@gmail.com>
2025-02-04 01:32:31 +0700

    Revert "Update resume and format data.json"
    This reverts commit 8e700ea60c0a20372caff8ba651e3377e0a8942f.

--- src/utils/Utils.jsx
+++ src/utils/Utils.jsx
@@ -1,5 +1,5 @@
 export function downloadCV() {
-  const resumeLink = 'Loc-Le.pdf';
+  const resumeLink = 'Loc Le Phat - Simple Resume.pdf';
   
   // Open a new window with the PDF
   window.open(resumeLink, '_blank');
